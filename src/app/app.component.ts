e96f7b275de56bfd21c3c1d621e6b1a306f8dfa3
Asia <joan.mk7@gmail.com>
2022-09-08 23:17:54 +0200

    add search films

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule  } from '@angular/forms';
 import { OmdbService } from './omdb.service';
 
 @Component({
@@ -9,13 +9,22 @@ import { OmdbService } from './omdb.service';
 })
 
 export class AppComponent {
+
   title = 'Task';
+  status: string | undefined;
+  error: string | undefined;
+  errorMessage: string | undefined;
+  requestFinished = false;
+  requestValid = false;
+  newForm = new FormGroup({
+    fieldVal: new FormControl("Guardians of the Galaxy")
+  });
 
-  public OmdbDetails: any;
+   public omdbDetails: any;
 
-  constructor(private omdbService: OmdbService) {
+   constructor(private omdbS: OmdbService) {
 
-  }
+   }
 
 
 
@@ -23,6 +32,32 @@ ngOnInit() {
 
 }
 
+ onSearch() {
+   let enteredPinCode = this.newForm.get('fieldVal')?.value;
+   this.omdbS.getOmdbDetails(enteredPinCode)
+   .subscribe(
+    (data : any) => {
+      console.log(data)
+      this.omdbDetails = data.Search;
+      console.log(data.Search)
+      this.status = data.Status;
+      this.requestFinished = true;
+      if (this.status === "404" || this.status === "Error") {
+        this.errorMessage = "Invalid Pincode " + enteredPinCode + "! Enter a valid one."
+        this.requestValid = false;
+      }
+      else {
+        this.errorMessage = "";
+        this.requestValid = true;
+      }
+    },
+    (error: any) => {
+        this.errorMessage = "Unexpected Error Occurred!";
+        this.requestValid = false;
+        console.log(this.errorMessage);
+      }
+    );
 
-}
 
+    }
+  }
--- src/app/omdb.service.ts
+++ src/app/omdb.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { OMDb } from './omdb';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class OmdbService {
 
   // API URI
-  private _url: string = "http://www.omdbapi.com/?apikey=879c7d9d&"
+  private _url: string = "https://www.omdbapi.com/?"
   constructor(private http: HttpClient) { }
 
-  getPostOfficeDetails(pincode: any): any {
-    return this.http.get<OMDb>(this._url + "s="+ pincode);
+  getOmdbDetails(pincode: any): any {
+
+    console.log(this._url)
+    return this.http.get<OMDb>(this._url + "s="+ pincode +"&apikey=879c7d9d");
   }
+
 }
